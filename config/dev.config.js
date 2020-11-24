27444bfffdb8703b6dea7d6c506356779f79cde3
jiangxiaoqiang <jiangtingqiang@gmail.com>
2020-11-24 07:48:34 -0500

    [feature] tweak

--- config/dev.config.js
+++ config/dev.config.js
@@ -10,7 +10,13 @@ module.exports = {
             loader: 'babel-loader',
             options: {
             presets: [
-                ['@babel/preset-env', { targets: "defaults" }]
+                [
+                    '@babel/preset-env',
+                    { targets: "defaults" }
+                ]
+            ],
+            plugins:[
+                "@babel/plugin-transform-arrow-functions", "@babel/plugin-proposal-class-properties"
             ]
             }
         }
