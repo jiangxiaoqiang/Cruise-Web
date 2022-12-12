c168874e41cac2edb65c386ee9ccd54dbc053f82
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-12-12 19:39:01 +0800

    feat: add footer with transparent

--- src/component/footer/Footer.tsx
+++ src/component/footer/Footer.tsx
@@ -2,17 +2,20 @@ import './Footer.css';
 import React from "react";
 import { Col, Divider, Row } from 'antd';
 
-
 const Footer: React.FC = (props) => {
   return (
-    <div id="footer">
-      <Divider className="cruisediv"></Divider>
-      <Row justify="center" align="middle">
-        <Col className="foot">
-          <div>© 2022 重庆红矮星科技有限公司</div>
-          <div>渝ICP备1700232号-6</div>
-        </Col>
-      </Row>
+    <div className="App-footer-float">
+      <div id="footer" className="App-footer">
+        <Divider></Divider>
+        <div>
+          <Row justify="center" align="middle">
+            <Col>
+              <div>© 2022 重庆红矮星科技有限公司</div>
+              <div>渝ICP备1700232号-6</div>
+            </Col>
+          </Row>
+        </div>
+      </div>
     </div>
   );
 }
