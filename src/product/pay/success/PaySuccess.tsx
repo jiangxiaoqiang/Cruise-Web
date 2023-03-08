4cdd810b69a97f42558651a482455c45cd4dbba5
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-03-09 02:43:16 +0800

    fix: tweak the url to imcompatible with newer browser

--- src/product/pay/success/PaySuccess.tsx
+++ src/product/pay/success/PaySuccess.tsx
@@ -11,7 +11,15 @@ const PaySuccess: React.FC = (props) => {
 
     const currentTime = dayjs().format('YYYY-MM-DD HH:mm:ss');
 
-    const parsed = queryString.parse(location.search);
+    if(location.search ==null){
+        return (<div className="pay-success-container">
+            <h1>支付失败！</h1>
+        </div>);
+    }
+    // 回调的链接可能是这样的格式https://read.poemhub.top/product/pay/success?orderId=619464963527516160&amp;payAmount=0.01&amp;sign_type=RSA2&amp;timestamp=2023-03-09%2002:21:56&amp;version=1.0
+    // &amp;是为了兼容老旧的浏览器
+    // 现代浏览器已经不再需要将 & 编码为 &amp;，但一些较旧版本的浏览器和遗留系统可能仍然需要这样做。
+    const parsed = queryString.parse(location.search.replace(/&amp;/g, '&'));
     debugger
     if(parsed != null && parsed.orderId && parsed.totalAmount){
       return (<div className="pay-success-container">
