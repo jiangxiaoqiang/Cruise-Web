fabd7918fb640eca5ad659ae25ec97ec5cbc578d
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-07-23 21:22:47 +0800

    refactor: tweak the chunk generate stratege

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import autoprefixer from 'autoprefixer';
 import { visualizer } from "rollup-plugin-visualizer";
+import { dependencies } from './package.json'
+
+const exclVendors = ['react', 'react-router-dom', 'react-dom'];
+function renderChunks(deps: Record<string, string>) {
+  let chunks = {}
+  Object.keys(deps).forEach((key) => {
+    if (exclVendors.includes(key)) return
+    chunks[key] = [key]
+  })
+  return chunks
+}
 
 export default defineConfig({
   plugins: [
@@ -23,13 +34,12 @@ export default defineConfig({
     }
   },
   build: {
+    sourcemap: false,
     outDir: "build",
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
+        manualChunks: {
+          ...renderChunks(dependencies),
         }
       }
     }
