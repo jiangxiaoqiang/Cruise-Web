d0459cca39528f46d06651ef9e1a257910c744d5
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-07-26 14:53:34 +0800

    feat: tweak the refresh token url

--- src/config/app/config-pro.ts
+++ src/config/app/config-pro.ts
@@ -3,12 +3,10 @@ let proConfigMap = new Map([
   ["loginUrl", "/read/user/login"],
   ["regUrl", "/read/user/reg"],
   ["baseAuthUrl","https://read.poemhub.top"],
-  ["accessTokenUrlPath","/read/auth/access-token/refresh"],
+  ["accessTokenUrlPath","/post/auth/access-token/refresh"],
   ["appHome","https://read.poemhub.top"],
   ["appId" , "oloLNAXvJu"],
-  ["cvBaseUrl","https://read.poemhub.top/cv/static/pdf/"],
-  ["defaultCvOrder","1,2,3,4,5"],
-  ["refreshUserUrl","/read/user/current-user"]
+  ["refreshUserUrl","/post/user/current-user"]
 ]); 
 
 export default proConfigMap;
